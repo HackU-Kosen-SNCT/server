e6de192eee925f4db6f24e8133bfa51e9b42879d
Shoma Kobayashi <sho1228.k@gmail.com>
2021-12-04 07:34:46 +0900

    remove: app controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LafModule } from './laf/laf.module';
 
 @Module({
   imports: [LafModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/laf/laf.controller.ts
+++ src/laf/laf.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('laf')
-export class LafController {}
+export class LafController {
+  @Get()
+  hello() {
+    return 'Hello World';
+  }
+}
