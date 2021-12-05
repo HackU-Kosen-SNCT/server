08b3e2abddd027af3c5e58e9f0970537eff4fe60
Shoma Kobayashi <sho1228.k@gmail.com>
2021-12-05 23:01:50 +0900

    update: user service test

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UpdateCategoryDto } from './user.dto';
@@ -10,9 +14,17 @@ export class UserService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
+  async getUserByRegistrant(registrant: string): Promise<User> {
+    const found = await this.userRepository.findOne(registrant);
+    if (!found) {
+      throw new NotFoundException();
+    }
+    return found;
+  }
+
   async updateCategory(updateCategoryDto: UpdateCategoryDto): Promise<User> {
     const { registrant, searching_category } = updateCategoryDto;
-    const user = await this.userRepository.findOne(registrant);
+    const user = await this.getUserByRegistrant(registrant);
     user.searching_category = searching_category;
 
     try {
