db50f63b99b0cee06a4fa0f85f04fc2fdc60aa32
Shoma Kobayashi <sho1228.k@gmail.com>
2021-12-07 01:50:35 +0900

    update: laf api swagger document

--- src/laf/laf.controller.ts
+++ src/laf/laf.controller.ts
@@ -1,5 +1,13 @@
-import { Body, Controller, Get, HttpStatus, Patch, Post } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+import { Body, Controller, Get, HttpCode, Patch, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+} from '@nestjs/swagger';
 import {
   GetLafItemsResponse,
   LafItem,
@@ -15,10 +23,13 @@ export class LafController {
   constructor(private lafService: LafService) {}
 
   @Get()
-  @ApiResponse({
-    status: HttpStatus.OK,
+  @ApiOperation({ summary: '登録された落とし物を全件取得するAPI' })
+  @ApiOkResponse({
     type: GetLafItemsResponse,
-    description: '落とし物を全件取得するAPI',
+    description: '成功時処理',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'サーバー側エラー時のレスポンス',
   })
   async getLafItems(): Promise<LafItem[]> {
     const items = await this.lafService.getLafItems();
@@ -30,29 +41,107 @@ export class LafController {
   }
 
   @Post()
-  @ApiResponse({
-    status: HttpStatus.CREATED,
+  @HttpCode(201)
+  @ApiOperation({ summary: '落とし物を登録するAPI' })
+  @ApiParam({
+    name: 'created_at',
+    required: true,
+    description: '登録時刻',
+  })
+  @ApiParam({
+    name: 'image_url',
+    required: true,
+    description: '画像URL',
+  })
+  @ApiParam({
+    name: 'longitude',
+    required: true,
+    description: '座標',
+  })
+  @ApiParam({
+    name: 'latitude',
+    required: true,
+    description: '座標',
+  })
+  @ApiParam({
+    name: 'detail',
+    required: false,
+    description: '詳しい場所などの詳細情報',
+  })
+  @ApiParam({
+    name: 'category',
+    required: true,
+    description: '落とし物のカテゴリ',
+  })
+  @ApiParam({
+    name: 'item_id',
+    required: true,
+    description: '登録された落とし物のID',
+  })
+  @ApiCreatedResponse({
     type: PostLafItemResponse,
-    description: '落とし物を登録するAPI',
+    description: '成功時処理',
+  })
+  @ApiBadRequestResponse({
+    description: 'category等の値が適切ではない場合に返されます(未実装)',
   })
   createLafItem(@Body() createLafItemDto: CreateLafItemDto): Promise<LafItem> {
     return this.lafService.createLafItem(createLafItemDto);
   }
 
+  // TODO: Response Type
   @Patch('/registrant')
-  @ApiResponse({
-    status: HttpStatus.NO_CONTENT,
-    description: '登録者を設定するAPI',
+  @ApiOperation({ summary: '落とし物の登録者を設定するAPI' })
+  @ApiParam({
+    name: 'registrant',
+    required: true,
+    description: '落とし物の登録者のID',
+  })
+  @ApiParam({
+    name: 'item_id',
+    required: true,
+    description: '落とし物のID',
+  })
+  @ApiOkResponse({
+    description: '成功時処理(未実装)',
+  })
+  @ApiNotFoundResponse({
+    description:
+      'item_id, registrantに適切でない(存在しない)値が入っていた場合に返す(未実装)',
   })
   registrant(@Body() registrantDto: RegistrantDto) {
     // TODO: registrant(LINEのuser_id)の値が存在するかのチェックもいるかも
     this.lafService.registrant(registrantDto);
   }
 
+  // TODO: Response Type
   @Patch('/receive')
-  @ApiResponse({
-    status: HttpStatus.NO_CONTENT,
-    description: 'メッセージの送信、受け取りの確認を行うAPI',
+  @ApiOperation({
+    summary: 'メッセージの送信・落とし物の受け取り処理を行うAPI',
+  })
+  @ApiParam({
+    name: 'received_at',
+    required: true,
+    description: '受け取った時刻',
+  })
+  @ApiParam({
+    name: 'message',
+    required: true,
+    description: '受け取った時に入力する感謝のメッセージ',
+  })
+  @ApiParam({
+    name: 'item_id',
+    required: true,
+    description: '落とし物のID',
+  })
+  @ApiOkResponse({
+    description: '成功時処理(未実装)',
+  })
+  @ApiBadRequestResponse({
+    description: 'messageが空の時などに返す(未実装)',
+  })
+  @ApiNotFoundResponse({
+    description: 'item_idが存在しない場合に返す(未実装)',
   })
   receive(@Body() receiveDto: ReceiveDto) {
     this.lafService.receive(receiveDto);
--- src/laf/laf.service.spec.ts
+++ src/laf/laf.service.spec.ts
@@ -1,18 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateLafItemDto, ReceiveDto, RegistrantDto } from './laf.dto';
+import { Laf } from './laf.entity';
 import { LafService } from './laf.service';
+
+const mockRepository = () => ({
+  find: jest.fn(),
+  save: jest.fn(),
+  findOne: jest.fn(),
+});
 
 describe('LafService', () => {
   let service: LafService;
+  let repository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LafService],
+      providers: [
+        LafService,
+        { provide: getRepositoryToken(Laf), useFactory: mockRepository },
+      ],
     }).compile();
 
-    service = module.get<LafService>(LafService);
+    service = await module.get<LafService>(LafService);
+    repository = await module.get(getRepositoryToken(Laf));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getLafItems', () => {
+    it('get all laf items', async () => {
+      repository.find.mockResolvedValue('mockLafs');
+      expect(repository.find).not.toHaveBeenCalled();
+
+      const result = await service.getLafItems();
+      expect(repository.find).toHaveBeenCalled();
+      expect(result).toEqual('mockLafs');
+    });
+  });
+
+  describe('createLafItem', () => {
+    it('create laf item', async () => {
+      const mockCreateLafItemDto: CreateLafItemDto = {
+        item_id: '20211205170917500',
+        category: 'clothing',
+        detail: 'スポーツタオル',
+        latitude: 82.28422,
+        longitude: 78.28292,
+        image_url:
+          'https://pbs.twimg.com/profile_images/1425448503010988032/p8GuVmXX_400x400.jpg',
+        created_at: new Date(),
+      };
+    });
+  });
+
+  describe('registrant', () => {
+    it('success register', async () => {
+      const mockRegistrantDto: RegistrantDto = {
+        item_id: '20211205170917500',
+        registrant: '82jsa982f90',
+      };
+    });
+  });
+
+  describe('receive', () => {
+    it('success receive', async () => {
+      const mockReceiveDto: ReceiveDto = {
+        item_id: '20211205170917500',
+        message: 'ありがとう！',
+        received_at: new Date(),
+      };
+    });
   });
 });
