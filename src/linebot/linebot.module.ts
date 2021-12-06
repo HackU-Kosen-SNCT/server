814a9fa06238ea7cdb5955f614c0fea5c0b7a6dd
Bayathy <misosoup0321@icloud.com>
2021-12-07 04:52:26 +0900

    Herokuにデプロイする準備

--- src/linebot/linebot.module.ts
+++ src/linebot/linebot.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module ,MiddlewareConsumer} from '@nestjs/common';
 import { LinebotService } from './linebot.service';
 import { LinebotController } from './linebot.controller';
+import { middleware, MiddlewareConfig } from '@line/bot-sdk';
 
 @Module({
   providers: [LinebotService],
   controllers: [LinebotController],
 })
-export class LinebotModule {}
+export class LinebotModule {
+  configure(consumer: MiddlewareConsumer) {
+
+    //Linebotのアクセストークン
+    const lineConfig: MiddlewareConfig = {
+      channelAccessToken: process.env.LINE_BOT_CHANNEL_TOKEN,
+      channelSecret: process.env.LINE_BOT_CHANNEL_SECRET,
+    };
+
+
+    // config を指定して LINE SDK の middleware を指定する
+    // 先にLinebot側のmiddlewareが働く必要があるため
+    consumer
+      .apply(middleware(lineConfig))
+      .forRoutes(LinebotController);
+  }
+}
