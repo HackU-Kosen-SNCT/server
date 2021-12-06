93fe3257101901ddb1dbd3f9740d128661058d56
Shoma Kobayashi <sho1228.k@gmail.com>
2021-12-06 21:54:39 +0900

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
 
   // swagger
   const options = new DocumentBuilder()
