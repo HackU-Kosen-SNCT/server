c69f3f7da4d90668732bb6cc964391a66478a3a1
Re-taro <aminokacho@gmail.com>
2021-12-08 22:42:40 +0900

    ✨ Add template message
    I don't know it works

--- src/linebot/linebot.service.ts
+++ src/linebot/linebot.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import '@line/bot-sdk';
+import type { TemplateMessage } from '@line/bot-sdk';
 @Injectable()
 export class LinebotService {
-  getHello(): string {
-    return 'Hello World!';
+  carouselMessage(): TemplateMessage {
+    const message: TemplateMessage = {
+      type: 'template',
+      altText: 'cannot display template message',
+      template: {
+        type: 'carousel',
+        columns: [
+          {
+            text: 'Hoge',
+            title: 'Fuga',
+            actions: [
+              {
+                type: 'uri',
+                label: 'See Wikipedia',
+                uri: 'https://ja.wikipedia.org/wiki/%E3%83%A1%E3%82%BF%E6%A7%8B%E6%96%87%E5%A4%89%E6%95%B0',
+              },
+            ],
+          },
+        ],
+      },
+    };
+    return message;
   }
 }
