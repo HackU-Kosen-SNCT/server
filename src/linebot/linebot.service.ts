2c8902c25f542eb38b35d541bc029402ba624cb1
Re-taro <aminokacho@gmail.com>
2021-12-08 22:48:59 +0900

    ✨ Add carousel message
    I don't know it works

--- src/linebot/linebot.service.ts
+++ src/linebot/linebot.service.ts
@@ -3,6 +3,7 @@ import '@line/bot-sdk';
 import { Client } from '@line/bot-sdk';
 import { LinebotConfigService } from 'src/config/linebot-config.service';
 import { ConfigService } from '@nestjs/config';
+import type { TemplateMessage } from '@line/bot-sdk';
 @Injectable()
 export class LinebotService {
   constructor(
@@ -20,5 +21,28 @@ export class LinebotService {
       type: 'text',
       text: message,
     });
+  }
+  carouselMessage(): TemplateMessage {
+    const message: TemplateMessage = {
+      type: 'template',
+      altText: 'cannot display template message',
+      template: {
+        type: 'carousel',
+        columns: [
+          {
+            text: 'Hoge',
+            title: 'Fuga',
+            actions: [
+              {
+                type: 'uri',
+                label: 'See Wikipedia',
+                uri: 'https://ja.wikipedia.org/wiki/%E3%83%A1%E3%82%BF%E6%A7%8B%E6%96%87%E5%A4%89%E6%95%B0',
+              },
+            ],
+          },
+        ],
+      },
+    };
+    return message;
   }
 }
