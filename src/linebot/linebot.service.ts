68691377001f761abbe8b999b9985ce79b551afd
Bayathy <misosoup0321@icloud.com>
2021-12-10 23:48:38 +0900

    カルーセルの削除

--- src/linebot/linebot.service.ts
+++ src/linebot/linebot.service.ts
@@ -64,10 +64,6 @@ export class LinebotService {
   }
 
 
-
-
-
-
   async SettingrichMenu() {
     const client = new Client(this.linebotConfigService.createLinebotOptions());
 
@@ -107,40 +103,5 @@ export class LinebotService {
       console.log(richMenuId))
   }
       
-      
-  carouselMessage(): TemplateMessage {
-    const message: TemplateMessage = {
-      type: 'template',
-      altText: 'cannot display template message',
-      template: {
-        type: 'carousel',
-        columns: [
-          {
-            text: 'Hoge',
-            title: 'Fuga',
-            actions: [
-              {
-                type: 'uri',
-                label: 'See Wikipedia',
-                uri: 'https://ja.wikipedia.org/wiki/%E3%83%A1%E3%82%BF%E6%A7%8B%E6%96%87%E5%A4%89%E6%95%B0',
-              },
-            ],
-          },
-          {
-            text: 'foo',
-            title: 'bar',
-            actions: [
-              {
-                type: 'uri',
-                label: 'See Wikipedia',
-                uri: 'https://ja.wikipedia.org/wiki/%E3%83%A1%E3%82%BF%E6%A7%8B%E6%96%87%E5%A4%89%E6%95%B0',
-              },
-            ],
-          },
-        ],
-      },
-    };
-    return message;
-  }
 }
 
