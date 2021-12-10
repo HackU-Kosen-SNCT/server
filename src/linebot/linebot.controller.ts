59a6dcff4f9156f15afa85490d74c76a01883bdf
Shoma Kobayashi <sho1228.k@gmail.com>
2021-12-10 13:11:54 +0900

    update: definition of handler

--- src/linebot/linebot.controller.ts
+++ src/linebot/linebot.controller.ts
@@ -1,8 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LinebotService } from './linebot.service';
 import { ConfigService } from '@nestjs/config';
-import { Client, TextEventMessage } from '@line/bot-sdk';
+import { WebhookEvent, WebhookRequestBody } from '@line/bot-sdk';
 import { LinebotConfigService } from 'src/config/linebot-config.service';
+import { UserService } from 'src/user/user.service';
 
 @Controller('linebot')
 export class LinebotController {
@@ -10,18 +11,29 @@ export class LinebotController {
     readonly linebotService: LinebotService,
     private configService: ConfigService,
     private linebotConfigService: LinebotConfigService,
+    private userService: UserService,
   ) {}
 
-  @Post('/laf')
+  @Post()
   // LINEプラットフォームから届くものを処理する
   // ex: 友達追加、メッセージの送信
-  create(@Body() destination: string, @Body() events: TextEventMessage[]) {
-    console.log(destination);
-    console.log(events);
-    const client = new Client(this.linebotConfigService.createLinebotOptions());
-    return client.pushMessage(
-      this.configService.get<string>('LINE_USER_ID'),
-      this.linebotService.carouselMessage(),
-    );
+  async handler(@Body() req: WebhookRequestBody) {
+    const events: WebhookEvent[] = req.events;
+    events.map((event) => {
+      switch (event.type) {
+        case 'follow':
+          // 友達追加時のイベント
+          // userテーブルへの追加処理を生やす
+          this.userService.registerUser(event.source.userId);
+          break;
+        default:
+          break;
+      }
+    });
+    // const client = new Client(this.linebotConfigService.createLinebotOptions());
+    // return client.pushMessage(
+    //   this.configService.get<string>('LINE_USER_ID'),
+    //   this.linebotService.carouselMessage(),
+    // );
   }
 }
